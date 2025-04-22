beca8beeb13e20344ef66050ca1f109bf7cf5863
chang180 <chang180@gmail.com>
2025-04-22 23:05:12 +0800

    Image 加入 likeBy 欄位

--- database/migrations/2025_04_22_134110_create_images_table.php
+++ database/migrations/2025_04_22_134110_create_images_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('vibe');
             $table->string('imagePath');
+            $table->string('likedBy')->nullable();
             $table->timestamps();
         });
     }
