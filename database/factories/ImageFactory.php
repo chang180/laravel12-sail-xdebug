61243868c89c0846a9846b1999e372b59bbc4bc9
chang180 <chang180@gmail.com>
2025-04-22 23:05:12 +0800

    Image 加入 likeBy 欄位

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Image;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -22,17 +23,27 @@ class ImageFactory extends Factory
             'name' => $this->faker->word . '.jpg',
             'vibe' => $this->faker->paragraph,
             'imagePath' => 'images/' . $this->faker->word . '.jpg',
+            'likedBy' => json_encode([]), // 默認為空陣列
         ];
     }
 
-    // 使用實際的檔案名稱
     public function fromExistingFile($filename, $path)
     {
         return $this->state(function (array $attributes) use ($filename, $path) {
+        // 獲取 1-5 個隨機現有用戶 ID
+        $count = rand(1, 5);
+        $existingUserIds = User::inRandomOrder()->limit($count)->pluck('id')->toArray();
+
+        // 如果沒有找到用戶，返回空陣列
+        if (empty($existingUserIds)) {
+            $existingUserIds = [];
+        }
+
             return [
                 'name' => $this->faker->name,
                 'vibe' => $this->faker->paragraph,
                 'imagePath' => $path,
+            'likedBy' => json_encode($existingUserIds), // 將用戶 ID 陣列轉換為 JSON 字符串
             ];
         });
     }
